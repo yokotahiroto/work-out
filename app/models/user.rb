3bd639d556968e90599ebbb2a55b5999e34701b7
yokotahiroto <hy_dkbs66mh@icloud.com>
2021-02-12 08:20:05 +0000

    [add]ゲストログイン追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,4 +36,13 @@ class User < ApplicationRecord
   def muscle_part
     muscles.map {|muscle| muscle.part }
   end
+  
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
+  
 end
