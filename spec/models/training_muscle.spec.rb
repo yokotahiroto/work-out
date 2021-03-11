efd713635824d5f6666f3afb11e118d135d486a5
yokotahiroto <hy_dkbs66mh@icloud.com>
2021-03-11 06:53:28 +0000

    [add]test

---
+++ spec/models/training_muscle.spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'TrainingCommentモデルのテスト', type: :model do
+   describe 'アソシエーションのテスト' do
+    context 'Muscleモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Training.reflect_on_association(:muscle).macro).to eq :belongs_to
+      end
+    end
+    
+    context 'Trainingモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Training.reflect_on_association(:training).macro).to eq :belongs_to
+      end
+    end
+  end
+end
